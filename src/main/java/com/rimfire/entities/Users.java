a7ad8d4596110415faa58dfa311d04acf9f75fc3
alvarezjo <jonathan.alvarez@axientcorp.com>
2023-08-17 17:19:58 -0500

    working

--- src/main/java/com/rimfire/entities/Users.java
+++ src/main/java/com/rimfire/entities/Users.java
@@ -3,12 +3,14 @@ package com.rimfire.entities;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import javax.validation.constraints.NotNull;
-import java.util.UUID;
+
+// import java.util.UUID;
+
 
 
 @MappedEntity
 public record Users (
-    @Id @NotNull UUID id,
+    @Id @NotNull Long id,
     @NotNull String username,
     @NotNull String password
 ) {
--- src/main/java/com/rimfire/repositories/UserRepository.java
+++ src/main/java/com/rimfire/repositories/UserRepository.java
@@ -4,10 +4,11 @@ import com.rimfire.entities.Users;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
-import java.util.UUID;
+// import java.util.UUID;
+
 
 @JdbcRepository(dialect = Dialect.POSTGRES) // specify the dialect here
-public interface UserRepository extends CrudRepository<Users, UUID> {
+public interface UserRepository extends CrudRepository<Users, Long> {
     // your repository methods
 
 
